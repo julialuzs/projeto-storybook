5e19cffb79d4361d3c69015b23f4134857dfbe06
Julia Luz dos Santos <julia.15081@gmail.com>
2024-10-08 23:41:18 -0300

    feat: adiciona button e configura stories

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,19 @@
 import type { Preview } from "@storybook/react";
 
+
+/* TODO: update import for your custom Material UI themes */
+
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
+    parameters: {
+        controls: {
+            expanded: true,
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
     },
-  },
+
 };
 
 export default preview;
